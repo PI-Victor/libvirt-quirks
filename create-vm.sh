83e4b071350fbe56476ba5cac260b3dd24aff283
PI-Victor <palade.ionut@gmail.com>
2018-05-20 11:34:57 +0200

    vms: improve create vm script

--- create-vm.sh
+++ create-vm.sh
@@ -1,24 +1,25 @@
 #!/usr/bin/env bash
 
-
-centos_image="https://cloud.centos.org/centos/7/images/CentOS-7-x86_64-GenericCloud-1802.qcow2"
 mem=${MEMORY:-1024}
 name=$1
-#if [[ ! -f /var/lib/libvirt/images/iso ]]; then
-#    wget $centos_image -P /var/lib/libvirt/images
-#fi
-genisoimage -o cloudinit/$name+config.iso -V cidata -r -J cloudinit/meta-data cloudinit/user-data
-sudo mv -f cloudinit/$name+config.iso /var/lib/libvirt/images/iso/
 
+fedora_image=$name-ArmetOS.qcow2
+fedora_image_path=/var/lib/libvirt/images/$fedora_image
+sudo cp /home/k8s-user/projects/cloudflavor/armetos/armetos/qemu/x86_64/soggy-sock/ArmetOS-Soggy-Sock-0.1.0-alpha.3.x86_64.qcow2 $fedora_image_path
+
+sudo genisoimage -o /var/lib/libvirt/images/$name-config.iso -V cidata -r -J cloudinit/meta-data cloudinit/user-data
 
 sudo sudo virt-install --name=$1 \
---cpu=host-passthrough \
---network type=bridge,source=br0,model=virtio \
---network=default \
---graphics spice,listen=172.16.15.100 \
---vcpus=2 \
---arch=x86_64 \
---memory=$mem \
---disk path=/var/lib/libvirt/images/$name.qcow2,format=qcow2,bus=virtio,cache=none,size=30 \
---disk /var/lib/libvirt/images/iso/$name+config.iso,device=cdrom \
---os-type=linux --os-variant=fedora18
+     --cpu host-passthrough \
+     --import \
+     --network type=bridge,source=br0,model=virtio \
+     --network default \
+     --graphics vnc,listen=172.16.15.100 \
+     --vcpus 2 \
+     --arch x86_64 \
+     --memory $mem \
+     --disk path=$fedora_image_path,bus=virtio,format=qcow2,cache=none,size=30 \
+     --disk /var/lib/libvirt/images/$name-config.iso,device=cdrom \
+     --os-type linux \
+     --os-variant fedora18 \
+     --console pty,target_type=serial
