e99e42c2b9e2aef09fe89b91bebc47e56d3f5838
PI-Victor <palade.ionut@gmail.com>
2018-05-20 11:35:26 +0200

    *: fix destroy script

--- destroy.sh
+++ destroy.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
 
-name=$1
+name=$1-ArmetOS.qcow2
+simple_name=$1
 
-sudo virsh destroy $name
-sudo virsh undefine $name
-sudo virsh vol-delete $name.qcow2 --pool default
+sudo virsh destroy $simple_name
+sudo virsh undefine $simple_name
+sudo virsh vol-delete $name --pool default
+sudo virsh vol-delete $simple_name-config.iso --pool default
