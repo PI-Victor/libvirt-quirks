15cd5cd05d38eb766a6c9f022c9bbc172eb41889
PI-Victor <palade.ionut@gmail.com>
2018-05-23 01:07:45 +0200

    *: merge create and destroy operations under one bash script

--- create-vm.sh
+++ create-vm.sh
@@ -1,25 +1,78 @@
 #!/usr/bin/env bash
 
-mem=${MEMORY:-1024}
-name=$1
+set -ef
 
-fedora_image=$name-ArmetOS.qcow2
-fedora_image_path=/var/lib/libvirt/images/$fedora_image
-sudo cp /home/k8s-user/projects/cloudflavor/armetos/armetos/qemu/x86_64/soggy-sock/ArmetOS-Soggy-Sock-0.1.0-alpha.3.x86_64.qcow2 $fedora_image_path
+# debug
+#set -v 
 
-sudo genisoimage -o /var/lib/libvirt/images/$name-config.iso -V cidata -r -J cloudinit/meta-data cloudinit/user-data
+pushd /var/lib/libvirt/images/iso
 
-sudo sudo virt-install --name=$1 \
-     --cpu host-passthrough \
-     --import \
-     --network type=bridge,source=br0,model=virtio \
-     --network default \
-     --graphics vnc,listen=172.16.15.100 \
-     --vcpus 2 \
-     --arch x86_64 \
-     --memory $mem \
-     --disk path=$fedora_image_path,bus=virtio,format=qcow2,cache=none,size=30 \
-     --disk /var/lib/libvirt/images/$name-config.iso,device=cdrom \
-     --os-type linux \
-     --os-variant fedora18 \
-     --console pty,target_type=serial
+if [ ! -f /var/lib/libvirt/images/iso/CentOS-7-x86_64-GenericCloud.qcow2 ]; then
+    sudo wget https://cloud.centos.org/centos/7/images/CentOS-7-x86_64-GenericCloud.qcow2.xz
+    sudo unxz CentOS-7-x86_64-GenericCloud.qcow2.xz
+fi
+popd
+
+create_vm() {
+    name=$1
+    path=$2
+    mem=$3
+    
+    sudo cp /var/lib/libvirt/images/iso/CentOS-7-x86_64-GenericCloud.qcow2 $path
+    
+    sed -i "s/instance-id.*/instance-id: $name/" cloudinit/meta-data
+    sed -i "s/local-hostname.*/local-hostname: $name/" cloudinit/meta-data
+    sudo genisoimage -o /var/lib/libvirt/images/$name-config.iso -V cidata -r -J cloudinit/meta-data cloudinit/user-data
+
+    sudo sudo virt-install --name=$name \
+	 --cpu host-passthrough \
+	 --import \
+	 --network type=bridge,source=br0,model=virtio \
+	 --network default \
+	 --graphics vnc,listen=172.16.15.100 \
+	 --vcpus 2 \
+	 --arch x86_64 \
+	 --memory $mem \
+	 --disk path=$2,bus=virtio,format=qcow2,cache=none,size=30 \
+	 --disk /var/lib/libvirt/images/$name-config.iso,device=cdrom \
+	 --os-type linux \
+	 --os-variant fedora18 \
+	 --console pty,target_type=serial
+}
+
+node_names=(k8s-master k8s-slave1 k8s-slave2)
+
+if [[ $1 = "create" ]]; then
+    for node in "${node_names[@]}"
+    do
+	echo "Creating VM - $node"
+	
+	if [[ $node != "k8s-master" ]]; then
+	    mem=4048
+	else
+	    mem=1024
+	fi
+	
+	qcow_image_path=/var/lib/libvirt/images/$node-CentOS7.qcow2
+	
+	create_vm $node $qcow_image_path $mem
+	
+	read -p "Continue (y/n)?" choice
+	case "$choice" in 
+	    y|Y ) echo "yes"
+		  continue;;
+	    n|N ) echo "no"
+		  exit;;
+	    * ) echo "invalid"
+		exit;;
+	esac
+    done
+else
+    domains=$(sudo virsh list --all | grep k8s | awk '{print $2}' | tail -n +1)
+    for domain in $domains; do
+	sudo virsh undefine $domain || true
+	sudo virsh destroy $domain || true
+	sudo virsh vol-delete $domain-CentOS7.qcow2 --pool default || true
+	sudo virsh vol-delete $domain-config.iso --pool default || true
+    done
+fi
--- destroy.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-name=$1-ArmetOS.qcow2
-simple_name=$1
-
-sudo virsh destroy $simple_name
-sudo virsh undefine $simple_name
-sudo virsh vol-delete $name --pool default
-sudo virsh vol-delete $simple_name-config.iso --pool default
